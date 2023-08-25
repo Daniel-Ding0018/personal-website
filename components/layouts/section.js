0260353f16b12a909a87c679bb835a9ff16b6078
Daniel Ding <ddin0018@student.monash.edu>
2023-08-26 02:58:02 +1000

    made build

--- components/layouts/section.js
+++ components/layouts/section.js
@@ -1,12 +1,12 @@
-import { motion } from 'framer-motion'
-import { chakra, shouldForwardProp } from '@chakra-ui/react'
+// import { motion } from 'framer-motion'
+// import { chakra, shouldForwardProp } from '@chakra-ui/react'
 
-const StyledDiv = chakra(motion.div, {
-  shouldForwardProp: prop => {
-    return shouldForwardProp(prop) || prop === 'transition'
-  }
-})
+// const StyledDiv = chakra(motion.div, {
+//   shouldForwardProp: prop => {
+//     return shouldForwardProp(prop) || prop === 'transition'
+//   }
+// })
 
-const Section = ({ children, delay = 0 }) => (
-  <StyledDiv initial={{ y: 10, opacity: 0 }}></StyledDiv>
-)
+// const Section = ({ children, delay = 0 }) => (
+//   <StyledDiv initial={{ y: 10, opacity: 0 }}></StyledDiv>
+// )
